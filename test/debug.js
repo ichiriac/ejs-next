5d7ac45569dc0a04b3e2f87c5efa12fffa55ca04
Ioan <i.chiriac@yahoo.com>
2019-02-20 13:48:49 +0100

    copyright headers

--- test/debug.js
+++ test/debug.js
@@ -1,3 +1,10 @@
+/**
+ * Copyright (C) 2019 Ioan CHIRIAC (MIT)
+ * @authors https://github.com/ichiriac/ejs2/graphs/contributors
+ * @url https://ejs.js.org
+ */
+"use strict";
+
 const lexer = require('../lib/parser/lexer');
 var lex = new lexer();
 lex.input(`
