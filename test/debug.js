18faa70e25f9a2d03529258900589e1bb307411c
Ioan <i.chiriac@yahoo.com>
2019-02-21 01:03:00 +0100

    wip

--- test/debug.js
+++ test/debug.js
@@ -5,20 +5,14 @@
  */
 "use strict";
 
-const lexer = require('../lib/parser/lexer');
-var lex = new lexer();
-lex.input(`
-<foo>
+const parser = require('../lib/ejs');
+const reader = new parser();
+const program = reader.eval(`
+<foo> <%%
 <%# comment %>
-<%_ if (foo) { _%>
+<%_ if (foo == "%>\\"%>") { _%>
   <%= bar %>
 <% } %>
 </foo>
 `);
-var tokens = [];
-while(true) {
-  var tok = lex.next();
-  tokens.push(tok);
-  if (tok[0] === lexer.tokens.T_EOF) break;
-}
-console.log(tokens);
+console.log(program);
