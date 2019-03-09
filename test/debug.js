d9044e71be2788b44e8ec3a923af77a3f40da231
Ioan <i.chiriac@yahoo.com>
2019-03-09 14:04:29 +0100

    improve the tests

--- test/debug.js
+++ test/debug.js
@@ -7,9 +7,26 @@
 
 const ejs = require('../lib/ejs');
 ejs.render(`
-  <%_ if (!wat) { _%>
-    ok <%= foo || "<>" %>
-  <%_ } _%>
+OK, so have fun! :D
+-------------------
+<%
+    var fruits = ["Apple", "Pear", "Orange", "Lemon"]
+      , random = " ".repeat(198).split("").map(x => Math.random())
+      ;
+%>
+
+These fruits are amazing:
+<%_ for(var i = 0; i < fruits.length; ++i) { %>
+  - <%=fruits[i]%>s
+<%_ } %>
+
+Let's see some random numbers:
+
+<% 
+  random.forEach(function(c, i) {
+%> <%= c.toFixed(10) + ((i + 1) % 6 === 0 ? "\\n": "") %><%
+  });
+%>
 `, { name: 'World' })
   .then(function(output) {
     console.log(output);
