cf4b77e3f105fcc0888bc65daa34c2c9cdfb656f
Ioan <i.chiriac@yahoo.com>
2019-03-31 15:13:00 +0200

    fix $ symbols

--- web.js
+++ web.js
@@ -19,12 +19,14 @@ var runtime = [];
 [
   "lib/lexer.js",
   "lib/transpile.js",
+  "lib/output.js",
   "lib/ejs.js"
 ].forEach(function(file) {
   full.push(extract(file));
 });
 
 [
+  "lib/output.js",
   "lib/ejs.js"
 ].forEach(function(file) {
   runtime.push(extract(file));
@@ -32,8 +34,8 @@ var runtime = [];
 
 
 var code = fs.readFileSync("lib/browser.js").toString();
-full = code.replace(/\/\/ @body/g, full.join(""));
-runtime = code.replace(/\/\/ @body/g, runtime.join(""));
+full = code.replace(/\/\/ @body/g, full.join("").replace(/\$/gm, '$$$$'));
+runtime = code.replace(/\/\/ @body/g, runtime.join("").replace(/\$/gm, '$$$$'));
 
 // compressed version
 var UglifyJS = require('uglify-js');
@@ -46,6 +48,11 @@ var runtime_min = UglifyJS.minify(runtime, {
 
 if (full_min.error) {
   console.error(full_min.error);
+  var src = full.split("\n").slice(
+    full_min.error.line - 5, full_min.error.line + 5
+  );
+  src[4] = "> " + src[4];
+  console.log(src.join("\n"));
 } else {
   fs.writeFileSync("dist/ejs.js", full);
   fs.writeFileSync("dist/ejs.runtime.js", runtime);
